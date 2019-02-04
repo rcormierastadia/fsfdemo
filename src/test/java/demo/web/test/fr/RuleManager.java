bffab94fecb821308ab7311727ea47d7ee5e96ae
rcormierastadia <rcormier70@gmail.com>
2019-02-04 16:01:58 +0000

    Update RuleManager.java

--- src/test/java/demo/web/test/fr/RuleManager.java
+++ src/test/java/demo/web/test/fr/RuleManager.java
@@ -17,7 +17,7 @@ public class RuleManager {
 	   
 	public void verifyRule(String ruleno)throws Throwable {
 		   
-		        String csvFile = "files/Astadia_FSF.csv";
+		        String csvFile = "files/Astadia_FSF_1002_Fail.csv";
 		        String line = "";
 		        String cvsSplitBy = ",";
 		        Rule x = new Rule();
